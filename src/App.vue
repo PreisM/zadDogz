c175755c56f92c5f48d7138dcd4a26740febcb67
Maciej Preis <maciej.preis@onet.eu>
2020-07-16 17:30:18 +0200

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,12 @@
 <template>
-  <div id="app">
-    <div class="log">
-      <k-input :label="'Login'"
-      type="text"
-      id="login"
-      v-model="login"/>
-    </div>
-    <div class="pass">
-      <k-input :label="'Password'" type="password" v-model="pass"/>
-    </div>
-    <div class="but">
-      <k-button v-model="send" @click="sendData">Submit</k-button>
-      <k-button @click="clean" icon="refresh" type="reset"></k-button>
-    </div>
+  <div>
+    <router-view />
   </div>
 </template>
 
-<script>
-import axios from 'axios';
+<script></script>
 
-const API = 'https://dogz.studio/api/login'
-
-export default {
-  data () {
-    return {
-      send: '',
-      login: '',
-      pass: '',
-      results: [],
-    };
-  },
-  methods: {
-    sendData() {
-      axios.post(`${API}`, {
-        email: this.login,
-        password: this.pass,
-      })
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-    },
-    clean() {
-      this.$data.login = "",
-      this.$data.pass = ""
-    }
-  },
-};
-</script>
-
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -59,12 +14,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-input[type=text] {
-}
-.k-button {
-  text-transform: none;
-  margin: 1em .5em;
 }
 </style>
---
+++ src/views/Reminder.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="reminderWrapper">
+    <div id="reminder">
+      <p>
+        Enter your email address. If it exists in the database, we will send
+        instructions on how to change the password
+      </p>
+      <k-input :label="'E-mail adress'" type="email" id="email" />
+    </div>
+    <k-button @click="thx">Send</k-button>
+    <k-button><router-link to="/">Return</router-link></k-button>
+    <div id="thx" v-if="step === 1">
+      Thank you. Check your e-mail account and click "return"
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Reminder",
+  data() {
+    return {
+      step: 0,
+    };
+  },
+  methods: {
+    thx() {
+      this.step = 1;
+    },
+  },
+};
+</script>
+
+<style scoped>
+#reminderWrapper {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#reminder {
+  margin-bottom: 1em;
+}
+
+.k-button {
+  text-transform: none;
+  margin: 1em 0.5em;
+}
+
+a {
+  text-decoration: none;
+  color: rgba(0, 0, 0, 0.87);
+}
+</style>
